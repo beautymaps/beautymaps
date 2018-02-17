1d977487f3805ab00bf90dd79c7b257100df6199
ctod <curiouscarine@Carines-MacBook-Pro.local>
2018-02-16 22:35:44 -0500

    added supply demand instructions and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Supply and Demand';
+  
+  //Create a model that represents a product obj
+  //Create a model that represents a supplier obj
+  //Create a list called demand that will hold a set of product objects
+  //Create a list called supply that will hold a set of supplier objects
+  //Create a function that given a product obj, will return the product and its children
+  //Create a function that given a suppliers properties will return the supplier obj
+  //Create a function that will add a supplier to the supply's list 
+  //Create a function that will add a product to the demand list 
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SupplyanddemandComponent } from './supplyanddemand/supplyanddemand.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SupplyanddemandComponent
   ],
   imports: [
     BrowserModule
