a1d3115a97a6c8b37f4486b66182882c404d1853
ctod <curiouscarine@Carines-MacBook-Pro.local>
2018-02-18 14:44:45 -0500

    added mvp instructions
    added routing and homeMap route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { SupplyanddemandComponent } from './supplyanddemand/supplyanddemand.component';
+import { HomeMapComponent } from './home-map/home-map.component';
 
+const appRoutes: Routes = [
+  {path: '', component: HomeMapComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SupplyanddemandComponent
+    SupplyanddemandComponent,
+    HomeMapComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
