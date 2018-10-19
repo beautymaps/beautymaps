a3febc5da92c48d451640174200973cbbcdb6e0d
ctod <curiouscarine@Carines-MacBook-Pro.local>
2018-10-18 22:21:30 -0400

    this is the default location

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -49,6 +49,8 @@ export class AddProductComponent implements OnInit {
         console.log('this is the location function  : ',pos )
         this.location = pos;
       });
+      this.location = this.locationService.location;
+      console.log('this is the location attribute', this.location);
       this.productList = this.db.list('/products');
       const product = this.db.list<Product>('/products');
       console.log('this is the:', this.location);        
